2bacd3a8034fac6a7fbe81244c8884b803a65841
khantlinthu789 <thuralwin1502@gmail.com>
2025-07-27 16:11:57 +0630

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,22 @@
+player.onChat("chickens", function (num1) {
+    if (num1 == 0) {
+        num1 = 2
+    }
+    for (let index = 0; index < num1; index++) {
+        mobs.spawn(WOLF, randpos(
+        pos(10, 10, 10),
+        pos(-10, 15, -10)
+        ))
+    }
+})new FloatGoal (p_25230_: this));
+
+new MeleeAttackGoal (p_25552_: this,
+
+new PandaRollGoal(p_29328_: this));
+
+new TurtleLayEggGoal( p_30276_: this,
+
+
+new GhastShootFireballGoal(p_32776_:
+
+new BreedGoal (p_25122: this, p_25123
